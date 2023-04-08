e23a4af65d059933d9a664e5a80ae901532e583b
rixm <erixm@proton.me>
2023-04-08 22:04:25 +0200

    Fixed spelling

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 1000
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "gola"
 
-#define GRID_WIDTH 500
-#define GRID_HEIGTH 500
+#define GRID_WIDTH WINDOW_WIDTH
+#define GRID_HEIGHT WINDOW_HEIGHT
 
 #define GAME_DELAY 16
 #define GAME_SEED 69
@@ -14,7 +14,7 @@ int main()
 {
     Application app(
         WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE,
-        GRID_WIDTH, GRID_HEIGTH,
+        GRID_WIDTH, GRID_HEIGHT,
         GAME_DELAY, GAME_SEED
     );
 
